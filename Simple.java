c1a90c976f6164acf0376fe7dfc1d0eb17d676fc
Yaxi Liu <yliu@yxliu.wireless.dyn.datasys.swri.edu>
2013-01-28 15:03:37 -0600

    basic files

---
+++ Simple.java
@@ -0,0 +1,8 @@
+import java.net.*;
+import java.io.*;
+
+public class Simple {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Let's see if Java will run\n");
+	}
+}
